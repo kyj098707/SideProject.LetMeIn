889278e8a1e7bfb51b48b5a1540cb6d60de0455d
Kim YoonJong <kyj098707@naver.com>
2023-10-19 10:02:14 +0900

    feat: 로그인 기능추가, 회원가입 로직 수정

--- back/api/serializers/users.py
+++ back/api/serializers/users.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
 
-class JoinSerializers(serializers.ModelSerializer):
+class JoinSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['pk','username','password','nickname']
@@ -13,5 +14,21 @@ class JoinSerializers(serializers.ModelSerializer):
         user = User(username = validated_data['username'],
                     password = validated_data['password'],
                     nickname = validated_data['nickname'])
+        user.set_password(validated_data['password'])
         user.save()
         return user
+        
+    
+class LoginSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+        data["username"] = self.user.username
+       
+
+        return data
+        
