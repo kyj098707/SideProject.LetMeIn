881ebff8d64fce78137658acc344d8538ba1e440
Kim YoonJong <kyj098707@naver.com>
2023-10-20 14:09:11 +0900

    feat: 포스팅 등록, 리스트 조회 API 생성

--- back/api/models.py
+++ back/api/models.py
@@ -14,3 +14,29 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = BaseUserManager()
     
+
+class Skill(models.Model):
+    name = models.CharField(max_length=20)
+    number = models.IntegerField(default=0)
+
+
+class Posting(models.Model):
+    title = models.CharField(max_length=100)
+    period = models.IntegerField()
+    content = models.TextField()
+    
+    representative_skill = models.CharField(max_length=104) 
+    skills = models.ManyToManyField(Skill)
+
+    total_member = models.IntegerField()
+    cur_member = models.IntegerField(default=1)
+
+    leader = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)    
+    
+
+    def __str__(self):
+        return self.title
+
--- back/api/urls.py
+++ back/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import users
+from .views import users,postings
 
 urlpatterns = [
     path('join/', users.join),
     path('login/', users.LoginView.as_view()),
+
+    path('posting/', postings.create_or_list),
 ]
